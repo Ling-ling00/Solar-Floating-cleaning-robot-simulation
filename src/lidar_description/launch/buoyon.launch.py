ed9522552b77cbba9eb910b73ea9e9c4b6a65b11
Ling-ling00 <mail123lingling@gmail.com>
2024-11-09 08:43:41 +0700

    first commit

---
+++ src/lidar_description/launch/buoyon.launch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+import xacro    
+    
+def generate_launch_description():
+    
+    pkg = get_package_share_directory('lidar_description')
+    
+    path_description = os.path.join(pkg,'urdf','square.xacro')
+    robot_desc_xml = xacro.process_file(path_description).toxml()
+    
+    parameters = [{'robot_description':robot_desc_xml}]
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                  executable='robot_state_publisher',
+                                  output='screen',
+                                  namespace='buoyon',
+                                  parameters=parameters
+    )
+
+    launch_description = LaunchDescription()
+    launch_description.add_action(robot_state_publisher)
+    
+    return launch_description
