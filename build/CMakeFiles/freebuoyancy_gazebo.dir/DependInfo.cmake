68078f2f4709f1919bde51ef5d02eb8aaa8e9158
Ling-ling00 <mail123lingling@gmail.com>
2024-11-11 20:05:10 +0700

    libraly

---
+++ build/CMakeFiles/freebuoyancy_gazebo.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ponwalai/freebuoyancy_gazebo/src/freebuoyancy.cpp" "CMakeFiles/freebuoyancy_gazebo.dir/src/freebuoyancy.cpp.o" "gcc" "CMakeFiles/freebuoyancy_gazebo.dir/src/freebuoyancy.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/freebuoyancy_gazebo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freebuoyancy_gazebo.dir/src/freebuoyancy.cpp.o"
+  "CMakeFiles/freebuoyancy_gazebo.dir/src/freebuoyancy.cpp.o.d"
+  "libfreebuoyancy_gazebo.pdb"
+  "libfreebuoyancy_gazebo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/freebuoyancy_gazebo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
